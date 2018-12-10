548a2bada78ec11a5f4d60ef7eb77d5cca9453b6
daxwann <daxwann@gmail.com>
2018-12-09 19:45:21 -0800

    add Board#populate

--- ruby/sudoku/board.rb
+++ ruby/sudoku/board.rb
@@ -1,11 +1,20 @@
 class Board
   def initialize(filename)
-    @board = Array.new(9) {Array.new(9)}
-    @file = open filename
+    @board = Array.new(9) {Array.new}
+    @filename = filename
   end
 
   def populate
-
+    file = File.open(@filename, "r")
+    file.each do |line|
+      row = 0
+      line.each do |val|
+        num = val.to_i
+        @board[row] << Tile.new(num, num == 0 ? false : true)
+      end
+      row += 1
+    end
+    file.close
   end
 
 end
