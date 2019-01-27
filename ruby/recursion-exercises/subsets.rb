8ae301e148df742cbf8cd4d16bbf217ddf5fe0b5
daxwann <daxwann@gmail.com>
2019-01-26 18:27:09 -0800

    debug subsets

--- ruby/recursion-exercises/subsets.rb
+++ ruby/recursion-exercises/subsets.rb
@@ -1,2 +1,17 @@
+require "byebug"
+
 # Write a method subsets that will return all subsets of an array.
 
+def subsets(array)
+    if array.empty?
+        return Array[[]]
+    end
+    last = array.pop
+    before_last = subsets(array)
+    new_arr = before_last.clone
+    before_last.each do |subset|
+        cpy = subset.clone
+        new_arr << (cpy << last)
+    end
+    return new_arr
+end
