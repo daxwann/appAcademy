e2920e98e4182bd4dd136acbca4e6f979284623d
daxwann <daxwann@gmail.com>
2019-02-10 14:56:09 -0800

    test dfs and bfs

--- ruby/PolyTreeNode/lib/00_tree_node.rb
+++ ruby/PolyTreeNode/lib/00_tree_node.rb
@@ -20,7 +20,7 @@ class PolyTreeNode
 
     def parent=(parent)
         unless @parent.nil?
-            @parent.children -= [self]
+            @parent.children.reject!{|child| child == self}
         end
         @parent = parent
         unless @parent.nil? || @parent.children.include?(self)
@@ -30,5 +30,37 @@ class PolyTreeNode
 
     def inspect
         @value.inspect
+    end
+
+    def add_child(child_node)
+        child_node.parent = self unless child_node.nil?
+    end
+
+    def remove_child(child_node)
+        return if child_node.nil?
+        if self.children.include?(child_node)
+            child_node.parent = nil
+        else
+            raise "Node not a child"
+        end
+    end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+        self.children.each do |child|
+            search_result = child.dfs(target_value)
+            return search_result unless search_result.nil?
+        end
+        nil
+    end
+
+    def bfs(target_value)
+        queue = [self]
+        until queue.empty?
+            ele = queue.shift
+            return ele if ele.value == target_value
+            ele.children.each {|child| queue << child}
+        end
+        nil
     end
 end
