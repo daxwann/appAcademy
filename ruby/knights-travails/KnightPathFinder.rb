311c2d0807598218a565e36ad12fb682230ebfc4
daxwann <daxwann@gmail.com>
2019-02-11 12:06:48 -0800

    add :considered array

--- ruby/knights-travails/KnightPathFinder.rb
+++ ruby/knights-travails/KnightPathFinder.rb
@@ -1,6 +1,10 @@
 require_relative "PolyTreeNode"
+require "byebug"
 
 class KnightPathFinder
+
+    attr_reader :root, :considered
+    
     DELTAS = [
         [-1, 2],
         [1, 2],
@@ -10,27 +14,44 @@ class KnightPathFinder
         [2, -1],
         [-1, -2],
         [1, -2]
-    ].freeze!
+    ].freeze
 
     def initialize(start)
-        @start = start
         @root = PolyTreeNode.new(start)
+        @considered = Array.new(start)
     end
 
-    def find_path(end)
-        
+    def find_path(end_pos)
+        queue = [self.root]
+        path = Array.new
+        until queue.empty?
+            current = queue.shift
+            return build_path(current) if current.value == end_pos
+            queue.concat(self.valid_moves(current))
+        end
+        nil
     end
 
-    def find_next_steps(current_pos)
-        x, y = current_pos
-        next_steps = Array.new
+    def build_path(current)
+        path = Array.new
+        until current.parent.nil?
+            path.unshift(current.value)
+            current = current.parent
+        end
+        path.unshift(current.value)
+        return path
+    end
+
+    def valid_moves(current_node)
         DELTAS.each do |delta|
+            x, y = current_node.value
             x += delta[0]
             y += delta[1]
-            if (x >= 0 && x < 8) && (y >= 0 && y < 8)
-                next_steps << [x, y]
-            end
+            next if (x < 0 || x > 7) || (y < 0 || y > 7)
+            next if self.considered.include?([x,y])
+            child = PolyTreeNode.new([x, y])
+            current_node.add_child(child)
         end
-        return next_steps
+        return current_node.children
     end
 end
