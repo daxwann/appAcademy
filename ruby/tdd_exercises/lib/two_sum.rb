6228e0a18cd045b506b3b108f83713b620b8204c
daxwann <daxwann@gmail.com>
2019-02-26 12:35:56 -0800

    add two sums

--- ruby/tdd_exercises/lib/two_sum.rb
+++ ruby/tdd_exercises/lib/two_sum.rb
@@ -1,4 +1,16 @@
 class Array
-    def two_sum
+  def two_sum
+    hash = Hash.new
+    pairs = []
+    self.each_with_index do |ele, idx|
+      next unless ele.is_a?(Integer)
+      if hash.has_value?(0 - ele)
+        matches = hash.select { |key, val| val == (0 - ele) }.keys
+        matches.each { |key| pairs << [key, idx] }
+      end
+      hash[idx] = ele
     end
+    return nil if pairs.empty?
+    return pairs.sort_by { |pair| pair.first }    
+  end
 end
--- ruby/tdd_exercises/spec/two_sum_spec.rb
+++ ruby/tdd_exercises/spec/two_sum_spec.rb
@@ -2,5 +2,26 @@ require 'rspec'
 require 'two_sum'
 
 describe 'Array#two_sum' do
+  subject(:arr) { Array.new }
+  context "when there are pairs of elements whose sum is zero" do
+    arr = [1, 2, -1, -2, 4, 5]
+    it "returns element pairs whose sum is zero" do
+      expect(arr.two_sum).to eq([[0, 2], [1, 3]])
+    end
+  end
+
+  context "when there are pairs of elements whose sum is zero" do
+    arr = [1, 2, 2, -1, -2, 4, -2, 5]
+    it "sorts all unique permutations of element pairs whose sum is zero" do
+      expect(arr.two_sum).to eq([[0, 3], [1, 4], [1, 6], [2, 4], [2, 6]])
+    end
+  end
+
+  context "when there are no pairs of elements whose sum is zero" do
+    arr = [1, 2, 3, 'a', 'b']
+    it "returns nil" do
+      expect(arr.two_sum).to eq(nil)
+    end
+  end    
     
 end
