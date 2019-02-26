7beeb44191f7b2f1c8e6047f06c8b4aa8c9da009
daxwann <daxwann@gmail.com>
2019-02-26 13:00:43 -0800

    add my_transpose

---
+++ ruby/tdd_exercises/spec/my_transpose_spec.rb
@@ -0,0 +1,10 @@
+require "rspec"
+require "my_transpose"
+
+describe "Array#my_transpose" do
+  context "when array is not a matrix" do
+  end
+
+  context "when matrix has various length arrays" do
+  end
+end
