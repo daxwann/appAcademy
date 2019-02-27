666335f73e37ad8e4920da91d1ef3fdc139713f6
daxwann <daxwann@gmail.com>
2019-02-27 10:44:01 -0800

    finish my_transpose

--- ruby/tdd_exercises/lib/my_transpose.rb
+++ ruby/tdd_exercises/lib/my_transpose.rb
@@ -1,4 +1,19 @@
 class Array
   def my_transpose
+    new_matrix = self.class.new
+    row_length = nil
+    self.each_with_index do |row, idx|
+      raise TypeError unless row.is_a?(Array)
+      if row_length.nil?
+        row_length = row.count
+        row_length.times { |i| new_matrix << self.class.new }
+      else
+        raise IndexError unless row.count == row_length
+      end
+      row.each_with_index do |col, idx|
+        new_matrix[idx] << col
+      end
+    end
+    return new_matrix
   end
 end
--- ruby/tdd_exercises/spec/my_transpose_spec.rb
+++ ruby/tdd_exercises/spec/my_transpose_spec.rb
@@ -2,9 +2,25 @@ require "rspec"
 require "my_transpose"
 
 describe "Array#my_transpose" do
+  subject(:arr) { Array.new }
   context "when array is not a matrix" do
+    arr = [1, 2, 3]
+    it "raises TypeError" do
+      expect {arr.my_transpose}.to raise_error(TypeError)
+    end
   end
 
   context "when matrix has various length arrays" do
+    arr = [[1] , [1, 2, 3], [4, 5, 6]]
+    it "raises IndexError" do
+      expect {arr.my_transpose}.to raise_error(IndexError)
+    end
+  end
+
+  context "when matrix has same length arrays" do
+    arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    it "transposes rows into columns" do
+      expect(arr.my_transpose).to eq([[1, 4, 7], [2, 5, 8], [3, 6, 9]])
+    end
   end
 end
