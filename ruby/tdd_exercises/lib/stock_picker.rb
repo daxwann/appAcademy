45f0490c522784a17de6221a653dbc52df8c42f2
daxwann <daxwann@gmail.com>
2019-03-02 00:35:34 -0800

    add more stock_picker

--- ruby/tdd_exercises/lib/stock_picker.rb
+++ ruby/tdd_exercises/lib/stock_picker.rb
@@ -1,11 +1,27 @@
 class Array
   def pick_stock
-    highest = 0
-    lowest = 1
-    max_profit = self[1] - self[0]
-    self.each_with_index do |price, day|
-      next if day < 2
-      
+    return nil if self.count < 2
+    max_profit = nil
+    lowest_day = nil
+    buy_sell = []
+    self.each_with_index do |sell_price, day|
+      next if day == 0
+      buy_price = self[day - 1]
+      next unless sell_price > buy_price
+      if lowest_day.nil?
+        lowest_day = day - 1
+      else
+        lowest_day = day - 1 if buy_price < self[lowest_day]
+      end
+      if max_profit.nil?
+        buy_sell << lowest_day
+        buy_sell << day
+      elsif max_profit < sell_price - self[lowest_day]
+        buy_sell[0] = lowest_day
+        buy_sell[1] = day
+      end
     end
+    return buy_sell unless buy_sell.empty?
+    nil
   end
 end
