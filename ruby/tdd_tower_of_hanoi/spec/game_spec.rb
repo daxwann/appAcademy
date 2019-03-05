1b4ada6f4c9253a2d0e5f1b8ac1e8edba6a51c1b
daxwann <daxwann@gmail.com>
2019-03-05 12:58:01 -0800

    add tests for board

--- ruby/tdd_tower_of_hanoi/spec/game_spec.rb
+++ ruby/tdd_tower_of_hanoi/spec/game_spec.rb
@@ -4,16 +4,36 @@ describe 'Game' do
   subject(:game) { Game.new }
 
   describe '#initialize' do
-    let(:stack) { double(:count => 3) }
+    let(:board) { double("board") }
 
-    it "start with a new stack of three columns" do
-      expect(game.stack.count).to eq(3)
+    it "starts with a new board" do
+      expect(game.stack).to_not eq(nil)
     end
   end
 
   describe '#won?' do
+    context 'all disks have been moved to third column' do
+      let(:board) { double(:cols => [[], [], [3, 2, 1]]) }
+
+      it "returns true" do
+        expect(game.won?).to eq(true)
+      end
+    end
+
+    context 'not all disks have been moved to third column' do
+      let(:board) { double(:cols => [[], [3], [2, 1]]) }
+
+      it "returns false" do
+        expect(game.won?).to eq(false)
+      end
+    end
   end
 
-  describe '#move' do
+  describe '#play' do
+    let(:board) { double(:cols => [[], [], [3, 2, 1]]) }
+
+    it "displays 'You did it!'" do
+      expect(STDOUT).to_receive(:puts).with('You did it!')
+    end
   end
 end
