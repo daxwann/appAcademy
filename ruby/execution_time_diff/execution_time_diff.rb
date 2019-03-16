b07592f117fb0f3dd20cbae92ac7a97c374b214a
daxwann <daxwann@gmail.com>
2019-03-16 01:21:11 -0700

    add largest_contiguous_subsum

--- ruby/execution_time_diff/execution_time_diff.rb
+++ ruby/execution_time_diff/execution_time_diff.rb
@@ -8,6 +8,20 @@ def my_min(arr)
   return smallest
 end
 
+def largest_contiguous_subsum(arr)
+  largest = current = arr.first
+ 
+  return arr.max if arr.all? { |num| num < 0 }
+
+  arr.drop(1).each do |num|
+    current = 0 if current < 0
+    current += num
+    largest = current if current > largest 
+  end
+
+  largest
+end
+
 # rspec
 
 describe '#my_min' do
@@ -16,4 +30,14 @@ describe '#my_min' do
   it "finds the smallest number in the list" do
     expect(my_min(list)).to eq(-5)
   end
+end
+
+describe '#largest_contiguous_subsum' do
+  list_1 = [5, 3, -7]
+  list_2 = [2, 3, -6, 7, -6, 7]
+  list_3 = [-5, -1, -3]
+
+  it { expect(largest_contiguous_subsum(list_1)).to eq(8) }
+  it { expect(largest_contiguous_subsum(list_2)).to eq(8) }
+  it { expect(largest_contiguous_subsum(list_3)).to eq(-1) }
 end
