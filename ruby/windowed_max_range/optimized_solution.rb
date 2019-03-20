386f5aa3b508a04098a50eafefd579648a4ce19d
daxwann <daxwann@gmail.com>
2019-03-20 00:16:57 -0700

    add MinMaxStack

--- ruby/windowed_max_range/optimized_solution.rb
+++ ruby/windowed_max_range/optimized_solution.rb
@@ -1,16 +1,16 @@
 require 'rspec'
 
 class MyQueue
-  def initialize
-    @store = []
+  def initialize(store = [])
+    @store = store 
   end
 
   def peek
-    @store.first
+    @store.last
   end
 
   def size
-    @store.count
+    @store.size
   end
 
   def empty?
@@ -26,9 +26,12 @@ class MyQueue
   end
 end
 
-class MyStack
-  def initialize
-    @store = []
+class MinMaxStack
+  attr_reader :max, :min
+  def initialize(store = [])
+    @store = store
+    @max = store.empty? ? nil : store.max
+    @min = store.empty? ? nil : store.min
   end
   
   def empty?
@@ -44,27 +47,52 @@ class MyStack
   end
 
   def pop
-    @store.pop
+    popped = @store.pop
+    @max = @store.max if popped == max
+    @min = @store.min if popped == min
   end
 
-  def push
-    @store.push
+  def push(val)
+    @store.push(val)
+    update_min_max(val)
   end    
+
+  private
+
+  def update_min_max(val)
+    @max = val if @max.nil? || val > @max
+    @min = val if @min.nil? || val < @min 
+  end
 end
 
 class StackQueue
   def initialize
-    @enqStack = Stack.new
-    @deqStack = Stack.new
+    @enq_stack = MyStack.new
+    @deq_stack = MyStack.new
+  end
+
+  def size
+    @enq_stack.size + @deq_stack.size
+  end
+
+  def empty?
+    @enq_stack.empty? && @deq_stack.empty?
   end
 
   def enqueue(val)
-    @enqStack.push(val)
+    @enq_stack.push(val)
   end
 
   def dequeue
+    if @deq_stack.empty?
+      @deq_stack.push(@enq_stack.pop) until @enq_stack.empty?
+    end
+
+    @deq_stack.pop
   end
 end
+
+class 
 # rspec
 
 describe '#windowed_max_range' do
