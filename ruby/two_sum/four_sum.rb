3d49ea4091837c90493ce2380a06c4b2540d7a9d
daxwann <daxwann@gmail.com>
2019-03-22 09:03:58 -0700

    test four_sum

--- ruby/two_sum/four_sum.rb
+++ ruby/two_sum/four_sum.rb
@@ -1,24 +1,32 @@
 require 'rspec'
 
 def four_sum?(arr, target_sum)
-  hash = Hash.new(0)
-  arr.each do |num|
-    hash[num] = 1
-  end
+  hash = Hash.new
+  two_sum_hash = Hash.new
+  three_sum_hash = Hash.new
 
-  arr.each_with_index do |num, idx|
-    three_sum = target_sum - num
-    arr.each_with_index do |num2, idx2|
-      next if idx2 == idx
-      two_sum_target = three_sum - num2
+  arr.each do |num|
+    # check if number is the last component of target sum
+    return true if three_sum_hash[target_sum - num]
+    # collect sums of three numbers
+    two_sum_hash.each_key do |key|
+      three_sum_hash[key + num] = true
     end
+    # collect sums of two numbers
+    hash.each_key do |key|
+      two_sum_hash[key + num] = true
+    end
+    # collect first component of target sum
+    hash[num] = true
   end
+  false
 end
+
 # rspec
 
 describe "four_sum?" do
   arr = [0, 1, 5, 7, 9, 13, 17]
 
-  it { expect(two_sum?(arr, 42)).to eq(true) }
-  it { expect(two_sum?(arr, 10)).to eq(false) }
+  it { expect(four_sum?(arr, 42)).to eq(true) }
+  it { expect(four_sum?(arr, 10)).to eq(false) }
 end
