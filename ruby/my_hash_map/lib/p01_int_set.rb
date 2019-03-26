80d33e53e6373ad0a97e06fcf770f8b82821c122
daxwann <daxwann@gmail.com>
2019-03-26 00:02:08 -0700

    complete IntSet

--- ruby/my_hash_map/lib/p01_int_set.rb
+++ ruby/my_hash_map/lib/p01_int_set.rb
@@ -41,18 +41,21 @@ class IntSet
     @store = Array.new(num_buckets) { Array.new }
   end
 
+  # time: O(n)
   def insert(num)
-    raise ArgumentError if self[num].include?(num)
+    raise ArgumentError if self.include?(num)
     self[num] << num
   end
 
+  # time: O(n)
   def remove(num)
-    raise ArgumentError unless self[num].include?(num)
+    raise ArgumentError unless self.include?(num)
     self[num].each_with_index do |ele, i|
       self[num].slice!(i) if ele == num
     end
   end
 
+  # time: O(n)
   def include?(num)
     self[num].include?(num)
   end
@@ -76,26 +79,47 @@ class ResizingIntSet
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
-
+  
+  # time: amortized avg is O(1) with random input; O(n) with pathological input.
   def insert(num)
+    return if self.include?(num)
+    resize! if count == num_buckets 
+    self[num] << num
+    @count += 1
   end
 
+  # time: amortized avg is O(1) with random input; O(n) with pathological input.
   def remove(num)
+    return unless self.include?(num)
+    self[num].each_with_index do |ele, i|
+      self[num].slice!(i) if ele == num
+      @count -= 1
+    end
   end
 
+  # time: amortized avg is O(1) with random input; O(n) with pathological input.
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
     @store.length
   end
 
+  #time: O(3n)
   def resize!
+    numbers = @store.flatten
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+    numbers.each do |num|
+     insert(num)
+    end 
   end
 end
