106da2419bfcca5c5697f628dcc9a898d6e27d3f
Dax Wann <daxwann@gmail.com>
2019-04-06 23:57:32 -0700

    add plays::find_by_title

--- sql/plays_demo/plays.rb
+++ sql/plays_demo/plays.rb
@@ -25,6 +25,34 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    data = PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        title = ?
+    SQL
+
+    data.map { |datum| Play.new(datum) }
+  end
+
+  def self.find_by_playwright(name)
+    data = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        plays.* 
+      FROM
+        plays
+      JOIN
+        playwrights ON plays.playwright_id = playwrights.id
+      WHERE
+        playwrights.name = ?
+    SQL
+
+    data.map { |datum| Play.new(datum) }
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -56,3 +84,5 @@ class Play
     SQL
   end
 end
+
+
