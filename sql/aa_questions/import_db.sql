8c32481ba6550c74b3c47491e188014df037c62c
Dax Wann <daxwann@gmail.com>
2019-04-09 00:12:59 -0700

    seed db

--- sql/aa_questions/import_db.sql
+++ sql/aa_questions/import_db.sql
@@ -45,6 +45,8 @@ CREATE TABLE question_likes (
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+/* seed users */
+
 INSERT INTO
   users (lname, fname)
 VALUES
@@ -52,6 +54,8 @@ VALUES
   ('Poehler', 'Amy'),
   ('Jobs', 'Steve'),
   ('Wann', 'Dax');
+
+/* seed questions */
 
 INSERT INTO
   questions (title, body, user_id)  
@@ -67,5 +71,65 @@ VALUES
       WHERE
         lname = 'Mooney'
     )
-  ),
+  ), (
+    'Python or Ruby?',
+    'Ennui marfa health goth, green juice crucifix tumeric fixie sriracha twee craft beer messenger bag. VHS leggings selfies, tofu quinoa deep v brooklyn helvetica try-hard paleo fanny pack austin flexitarian. Af biodiesel pickled asymmetrical affogato hexagon craft beer, lumbersexual franzen food truck. Fashion axe bitters hell of, ennui yuccie polaroid williamsburg aesthetic meditation authentic photo booth gentrify etsy franzen.',
+    (
+      SELECT
+        id
+      FROM
+        users
+      WHERE
+        lname = 'Wann'
+    )
+  );
 
+/* seed question follows */
+
+INSERT INTO
+  question_follows (question_id, user_id)
+VALUES
+  (1, 2),
+  (2, 3),
+  (1, 3);
+
+/* seed replies */
+
+INSERT INTO
+  replies (body, reply_id, user_id, question_id)
+VALUES
+  (
+    'Banh mi bushwick YOLO migas, brunch activated charcoal deep v messenger bag art party pug banjo dreamcatcher. Pabst gentrify locavore, roof party wayfarers small batch snackwave chia raclette asymmetrical vexillologist. Cliche irony paleo bitters humblebrag tilde four dollar toast cold-pressed farm-to-table salvia narwhal lumbersexual retro. Kickstarter glossier cronut viral. Flexitarian waistcoat lyft freegan chartreuse.',
+    NULL,
+    3,
+    1
+  ),
+  (
+    'Put a bird on it mustache farm-to-table, gentrify health goth butcher 3 wolf moon jean shorts green juice. Tumblr tousled pop-up pour-over, food truck truffaut vape next level. Williamsburg synth hexagon mixtape salvia. Chicharrones tousled artisan taxidermy, gentrify butcher shaman. Jean shorts craft beer meh, artisan YOLO beard poke af swag tattooed tofu yr.',
+    1,
+    4,
+    1
+  ),
+  (
+    'Distillery vice blog fingerstache photo booth PBR&B semiotics tilde la croix 3 wolf moon franzen direct trade. Gochujang intelligentsia hell of, organic 3 wolf moon selvage keffiyeh brooklyn narwhal coloring book typewriter truffaut iceland. Irony unicorn tote bag godard blue bottle truffaut food truck pitchfork organic leggings tumblr man braid etsy everyday carry. Skateboard actually poke selfies health goth. Trust fund mixtape mlkshk meggings, twee truffaut green juice. Gochujang vice pickled, crucifix street art banjo blue bottle. Schlitz cornhole af banh mi green juice health goth.',
+    NULL,
+    2,
+    2
+  ),
+  (
+    'Lorem ipsum dolor amet dIY beard yuccie kale chips plaid literally raclette hashtag church-key XOXO vegan typewriter. Af la croix taxidermy green juice slow-carb pork belly fashion axe bicycle rights ugh. Man bun wayfarers actually, mixtape leggings vegan raw denim ugh authentic you probably haven''t heard of them seitan distillery. Cred trust fund poutine, scenester snackwave pickled leggings tofu vaporware direct trade mlkshk franzen cardigan shabby chic. Master cleanse hammock pop-up single-origin coffee.',
+    3,
+    1,
+    2
+  );
+
+/* seed likes */
+
+INSERT INTO
+  question_likes (question_id, user_id)
+VALUES
+  (1, 4),
+  (1, 2),
+  (1, 3),
+  (2, 1),
+  (2, 4);
