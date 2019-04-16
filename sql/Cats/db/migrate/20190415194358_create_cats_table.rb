ac4ca10f9f02540189dc35e96d1dcfc229b79929
Dax Wann <daxwann@gmail.com>
2019-04-15 23:28:05 -0700

    migration

--- sql/Cats/db/migrate/20190415194358_create_cats_table.rb
+++ sql/Cats/db/migrate/20190415194358_create_cats_table.rb
@@ -1,6 +1,8 @@
 class CreateCatsTable < ActiveRecord::Migration[5.2]
   def change
-    create_table :cats_tables do |t|
+    create_table :cats do |t|
+      t.string :name, null: false
+      t.timestamps
     end
   end
 end
