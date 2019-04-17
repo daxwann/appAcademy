3cef81cd8a0dc46f68992138050ef6150ce6696f
Dax Wann <daxwann@gmail.com>
2019-04-16 23:38:28 -0700

    create house table'

--- sql/Cats/db/schema.rb
+++ sql/Cats/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_183236) do
+ActiveRecord::Schema.define(version: 2019_04_16_194418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2019_04_16_183236) do
     t.datetime "updated_at", null: false
     t.string "color"
     t.integer "house_id"
+  end
+
+  create_table "houses", force: :cascade do |t|
+    t.string "type", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "toys", force: :cascade do |t|
