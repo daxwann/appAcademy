227c1bb19afd490c72653c974204e7142d860215
Dax Wann <daxwann@gmail.com>
2019-04-27 16:46:43 -0700

    add polls_app

---
+++ sql/polls_app/app/models/response.rb
@@ -0,0 +1,31 @@
+class Response < ApplicationRecord
+  belongs_to :answer_choice,
+    primary_key: :id,
+    foreign_key: :answer_choice_id,
+    class_name: :AnswerChoice
+
+  belongs_to :respondent,
+    primary_key: :id,
+    foreign_key: :respondent_id,
+    class_name: :User
+
+  has_one :question
+    through: :answer_choice,
+    source: :question
+
+  def sibling_responses
+    self.question.responses.where.not(id: self.id)
+  end
+
+  def respondent_already_answered
+    sibling_responses.exists?(respondent_id: self.respondent_id)
+  end
+
+  private
+
+  def not_duplicate_response
+    if respondent_already_answered
+      errors[:respondent_id] << "cannot vote twice"
+    end
+  end
+end
