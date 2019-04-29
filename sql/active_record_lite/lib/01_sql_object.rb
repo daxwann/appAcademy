5c1252c64458a26960a4a4d3817adf3e2fcf2bc2
Dax Wann <daxwann@gmail.com>
2019-04-29 12:58:08 -0700

    add #initialize

--- sql/active_record_lite/lib/01_sql_object.rb
+++ sql/active_record_lite/lib/01_sql_object.rb
@@ -6,17 +6,39 @@ require 'active_support/inflector'
 class SQLObject
   def self.columns
     # ...
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+        0
+    SQL
+    cols.map!(&:to_sym)
+    @columns = cols
   end
 
   def self.finalize!
+    self.columns.each do |name|
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
     # ...
+    @table_name = table_name
   end
 
   def self.table_name
     # ...
+    @table_name || self.name.tableize
   end
 
   def self.all
@@ -33,10 +55,19 @@ class SQLObject
 
   def initialize(params = {})
     # ...
+    params.each do |attr_name, value|
+      attr_name = attr_name.to_sym
+      if self.class.columns.include?(attr_name)
+        self.send("#{attr_name}=", value)
+      else
+        raise "unknown attribute '#{attr_name}'"
+      end
+    end
   end
 
   def attributes
     # ...
+    @attributes ||= {}
   end
 
   def attribute_values
