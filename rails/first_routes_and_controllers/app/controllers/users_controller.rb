45289c71a96a3da25dbf974b10c447de9cbadaed
Dax Wann <daxwann@gmail.com>
2019-05-04 15:11:22 -0700

    finish first routes and controllers

--- rails/first_routes_and_controllers/app/controllers/users_controller.rb
+++ rails/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,13 +1,57 @@
 class UsersController < ApplicationController
   def index
-    render plain: "I'm in the index action!"
-  end
-
-  def create
-    render json: params
+    users = User.all
+    hash = Hash.new
+    users.each do |user|
+      hash[user.id] = user 
+    end
+    render json: hash
   end
 
   def show
-    render json: params
+    user = User.find(params[:id])
+
+    if user
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
+
+  def create
+    user = user_params    
+
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+
+    if user.destroy
+      render json: user 
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update_attributes(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end
