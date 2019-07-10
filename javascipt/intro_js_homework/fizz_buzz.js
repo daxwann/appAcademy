3b597648c1fb6164c967946425c274800a0c2111
Dax Wann <daxwann@gmail.com>
2019-07-10 15:36:59 -0700

    add fizzBuzz exercise

---
+++ javascipt/intro_js_homework/fizz_buzz.js
@@ -0,0 +1,9 @@
+/*Define a function fizzBuzz(array) that takes an array and returns a new array of every number in the array that is divisible by either 3 or 5, but not both.*/
+
+const fizzBuzz = (array) => {
+  const arr = array.filter(num => num % 3 == 0 || num % 5 == 0);
+  return arr;
+}
+
+console.log(fizzBuzz([1, 3, 5, 6, 7, 111])); // [3, 5, 6, 111]
+console.log(fizzBuzz([15, 25, 12, -12, 2])); // [15, 25, 12, -12]
