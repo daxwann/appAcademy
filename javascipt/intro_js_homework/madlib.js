b982f890241784cc1ea8b9edcabf94f7f9bbbbea
Dax Wann <daxwann@gmail.com>
2019-07-10 15:20:02 -0700

    add isSubstring exercise

--- javascipt/intro_js_homework/madlib.js
+++ javascipt/intro_js_homework/madlib.js
@@ -1,3 +1,5 @@
+/*Write a function that takes three strings - a verb, an adjective, and a noun - uppercases and interpolates them into the sentence "We shall VERB the ADJECTIVE NOUN". Use ES6 template literals.*/
+
 const madLib = (verb, adj, noun) => {
   return `We shall ${verb} the ${adj} ${noun}!!!`;
 }
