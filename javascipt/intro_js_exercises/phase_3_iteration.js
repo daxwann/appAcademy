9c24493d10f97c575a77ebb4fd36b9752557d153
Dax Wann <daxwann@gmail.com>
2019-07-12 10:48:09 -0700

    add Array#bubbleSort

---
+++ javascipt/intro_js_exercises/phase_3_iteration.js
@@ -0,0 +1,22 @@
+/* Array#bubbleSort - receives an array, returns a sorted array by implementing bubble sort sorting algorithm */
+
+Array.prototype.bubbleSort = function() {
+  let self = this;
+  let sorted = false;
+  let cycle = 0;
+
+  while (sorted == false) {
+    sorted = true;
+    for (let i = 0; i < (self.length - 1 - cycle); i++) {
+      if (self[i] > self[i + 1]) {
+        let temp = self[i];
+        self[i] = self[i + 1];
+        self[i + 1] = temp; 
+        sorted = false;
+      }
+    }
+    cycle++;
+  }
+
+  return self;
+};
