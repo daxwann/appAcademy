f0b6289047c569185c457ab57d2b30e9224bd0fc
Dax Wann <daxwann@gmail.com>
2019-07-11 18:20:53 -0700

    add Array#myEach and Array#myMap

---
+++ javascipt/intro_js_exercises/phase_2_enumerable.js
@@ -0,0 +1,34 @@
+/* Array#myEach(callback) - receives a callback function and executes the callback for each element in the array */
+
+Array.prototype.myEach = function(callback) {
+  let self = this;
+
+  for (let i = 0; i < self.length; i++) {
+    callback(self[i]);
+  }
+
+  return undefined;
+};
+
+/*Array#myMap(callback) - receives a callback function, returns a new array of the results of calling the callback function on each element of the array*/
+
+Array.prototype.myMap = function(callback) {
+  let self = this;
+  let newArr = [];
+
+  self.forEach((ele) => {
+    newArr.push(callback(ele));
+  });
+
+  return newArr;
+};
+
+/* Array#myReduce(callback[, initialValue]) - (like Ruby's Array#inject) receives a callback function, and optional initial value, returns an accumulator by applying the callback function to each element and the result of the last invocation of the callback (or initial value if supplied) */
+
+Array.prototype.myReduce = function(callback, initial = 0) {
+  
+};
+
+
+
+
