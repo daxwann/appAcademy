165152efa9963d590e336485565be459ae465873
Dax Wann <daxwann@gmail.com>
2019-07-13 12:12:10 -0700

    add exponent recursion exercises

--- javascipt/intro_js_exercises/phase_4_recursion.js
+++ javascipt/intro_js_exercises/phase_4_recursion.js
@@ -26,3 +26,27 @@ const sumRec = function(arr) {
   if (arr.length > 1)
     return arr[0] + sumRec(arr.slice(1));
 };
+
+/* exponent(base, exp) - receives a base and exponent, returns the base raise to the power of the exponent (base ^ exp) */
+
+/* recursion 1 */
+const exp1 = function(base, exp) {
+  if (exp == 0)
+    return 1;
+
+  return base * exp1(base, exp - 1);
+};
+
+/* recursion 2 */
+const exp2 = function(base, exp) {
+  if (exp == 0)
+    return 1;
+  
+  if (exp == 1)
+    return base;
+
+  if (exp % 2 == 0)
+    return exp2(base, exp / 2) ** 2;
+  else
+    return base * (exp2(base, (exp - 1) / 2) ** 2);
+};
