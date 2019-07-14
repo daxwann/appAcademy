62d8aa482b0b7831ff0ca65f61108cb89dd16567
Dax Wann <daxwann@gmail.com>
2019-07-14 16:22:02 -0700

    add Course#conflictsWith

--- javascipt/intro_js_exercises/phase_6_students_and_courses.js
+++ javascipt/intro_js_exercises/phase_6_students_and_courses.js
@@ -1,3 +1,4 @@
+// Student constructor and prototypes
 const Student = function(lastName, firstName) {
   this.lastName = lastName;
   this.firstName = firstName;
@@ -8,9 +9,19 @@ Student.prototype.name = function() {
   return `${this.firstName} ${this.lastName}`;
 };
 
+Student.prototype.hasConflict = function(course) {
+  for (let i = 0; i < this.courses.length; i++) {
+    if (this.courses[i].conflictsWith(course))
+      return true;
+  }
+  return false;
+};
+
 Student.prototype.enroll = function(course) {
   if (course.students.includes(this)) {
     console.log(`Student already enrolled in ${course.name}`);
+  } else if (this.hasConflict(course)) {
+    console.log(`${course.name} has conflict with another enrolled course`);
   } else {
     course.students.push(this);
     this.courses.push(course);
@@ -19,21 +30,43 @@ Student.prototype.enroll = function(course) {
 };
 
 Student.prototype.courseLoad = function() {
-  return this.courses.reduce((courseLoad, course) => {
-    if (courseLoad.keys.includes(course.department))
-      courseLoad[course.department] = courseLoad[course.department] + course.credits;
+  let courseLoadHash = {};
+  this.courses.forEach((course) => {
+    if (Object.keys(courseLoadHash).includes(course.dept))
+      courseLoadHash[course.dept] = courseLoadHash[course.dept] + course.credits;
     else
-      courseLoad[course.department] = course.credits;
-  }, {});
+      courseLoadHash[course.dept] = course.credits;
+  });
+
+  return courseLoadHash;
 }
 
-const Course = function(name, dept, credits) {
+// Course constructor and prototypes
+const Course = function(name, dept, credits, timeBlock, daysArr) {
   this.name = name;
   this.dept = dept;
   this.credits = credits;
   this.students = [];
+  this.daysArr = daysArr;
+  this.timeBlock = timeBlock;
 };
 
 Course.prototype.addStudent = function(student) {
   student.enroll(this); 
+};
+
+Course.prototype.conflictsWith = function(course) {
+  const doesDaysConflict = () => {
+    for (let i = 0; i < this.daysArr.length; i++) {
+      if (course.daysArr.includes(this.daysArr[i])) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  if (this.timeBlock == course.timeBlock && doesDaysConflict())
+    return true;
+  else
+    return false;
 };
