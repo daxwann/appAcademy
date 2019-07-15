86a26541deb8742b2391fb5bf5b04960876cc778
Dax Wann <daxwann@gmail.com>
2019-07-14 17:12:07 -0700

    fix String#substrings

--- javascipt/intro_js_exercises/phase_3_iteration.js
+++ javascipt/intro_js_exercises/phase_3_iteration.js
@@ -9,9 +9,12 @@ Array.prototype.bubbleSort = function() {
     sorted = true;
     for (let i = 0; i < (self.length - 1 - cycle); i++) {
       if (self[i] > self[i + 1]) {
+        /*
         let temp = self[i];
         self[i] = self[i + 1];
         self[i + 1] = temp; 
+        */
+        [self[i], self[i + 1]] = [self[i + 1], self[i]];
         sorted = false;
       }
     }
@@ -24,7 +27,13 @@ Array.prototype.bubbleSort = function() {
 /* String#substrings - receives a string, returns an array of all substrings */
 
 String.prototype.substrings = function() {
-  let self = this;
+  let substr = [];
+  
+  for (let start = 0; start < this.length; start++) {
+    for (let end = start + 1; end <= this.length; end++) {
+      substr.push(this.slice(start, end));
+    }
+  }
 
-  return self.split(" ")
+  return substr;
 };
