3a3c7423ed3272aea222af195b2b4094eed223ca
Dax Wann <daxwann@gmail.com>
2019-07-23 12:03:19 -0700

    add comments and use JSON for completion display

--- javascipt/intro_callbacks_exercises/absurd_bubblesort.js
+++ javascipt/intro_callbacks_exercises/absurd_bubblesort.js
@@ -28,6 +28,8 @@ function absurdBubbleSort(arr, sortCompletionCallback) {
 }
 
 function askIfGreaterThan(el1, el2, callback) {
+  // Prompt user to tell us whether el1 > el2; pass true back to the
+  // callback if true; else false.
   reader.question(`Is ${el1} greater than ${el2}? `, (res) => {
     if (res == 'yes') {
       callback(true);
@@ -41,10 +43,16 @@ function askIfGreaterThan(el1, el2, callback) {
 }
 
 function innerBubbleSortLoop(arr, i, cycle, madeAnySwaps, outerBubbleSortLoop) {
-  if (i === arr.length - 1 - cycle) {
+  // If (i == arr.length - 1), call outerBubbleSortLoop, letting it
+  //  know whether any swap was made.
+    if (i === arr.length - 1 - cycle) {
     cycle++;
     outerBubbleSortLoop(madeAnySwaps, cycle);
   } else if (i < arr.length - 1 - cycle) {
+  // Else, use `askIfGreaterThan` to compare `arr[i]` and `arr[i +
+  //    1]`. Swap if necessary. Call `innerBubbleSortLoop` again to
+  //    continue the inner loop. You'll want to increment i for the
+  //    next call, and possibly switch madeAnySwaps if you did swap.
     askIfGreaterThan(arr[i], arr[i + 1], (isGreaterThan) => {
       if (isGreaterThan) {
         [arr[i + 1], arr[i]] = [arr[i], arr[i + 1]];
@@ -58,5 +66,5 @@ function innerBubbleSortLoop(arr, i, cycle, madeAnySwaps, outerBubbleSortLoop) {
 }
 
 absurdBubbleSort([20, 3, 2, 4, 1], (arr) => {
-  console.log(`${arr} is sorted`);
+  console.log(`${JSON.stringify(arr)} is sorted`);
 });
