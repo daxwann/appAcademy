6c981b07d48efde0bd786a0ee00ad711cd86c707
Dax Wann <daxwann@gmail.com>
2019-07-23 12:53:32 -0700

    complete function#myThrottle

---
+++ javascipt/intro_callbacks_exercises/my_throttle.js
@@ -0,0 +1,69 @@
+// Write your own myThrottle(inteval) function on the Function.prototype. 
+// myThrottle should take an interval as an argument and return 
+// a "throttled" version of the original function that can only be invoked 
+// every interval milliseconds. 
+
+Function.prototype.myThrottle = function(interval) {
+  const fn = this;
+  let tooSoon = false;
+ 
+  return () => {
+    if (tooSoon === true) {
+      return;
+    }
+
+    setTimeout(() => {
+      tooSoon = false;
+    }, interval);
+
+    fn(arguments);
+    tooSoon = true;
+  };
+};
+
+// test
+
+class Neuron {
+  fire() {
+    console.log("Firing!");
+  }
+};
+
+const neuron = new Neuron;
+// When we create a new Neuron,
+// we can call #fire as frequently as we want
+
+// The following code will try to #fire the neuron every 10ms. Try it in the console:
+const interval = setInterval(() => {
+  neuron.fire();
+}, 10);
+
+// You can use clearInterval to stop the firing:
+clearInterval(interval);
+
+// Using Function#myThrottle, we should be able to throttle
+// the #fire function of our neuron so that it can only fire
+// once every 500ms:
+
+neuron.fire = neuron.fire.myThrottle(500);
+
+const interval = setInterval(() => {
+  neuron.fire();
+}, 10);
+
+// This time, if our Function#myThrottle worked correctly,
+// the Neuron#fire function should only be able to execute
+// every 500ms, even though we're still trying to invoke it
+// every 10ms!
+
+// If we want this behavior for ALL neurons, we can do the same logic in the constructor:
+
+class Neuron {
+  constructor() {
+    this.fire = this.fire.myThrottle(500);
+  }
+
+  fire() {
+    console.log("Firing!");
+  }
+};
