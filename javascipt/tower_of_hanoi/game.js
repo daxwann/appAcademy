52d413bccf4c673397691055be366c36963cbea8
Dax Wann <daxwann@gmail.com>
2019-07-25 15:11:07 -0700

    add playScript

--- javascipt/tower_of_hanoi/game.js
+++ javascipt/tower_of_hanoi/game.js
@@ -1,18 +1,11 @@
-const readline = require('readline');
 const board = require('./board.js');
-
-const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
 
 class Game {
   constructor() {
     this.board = new board;
   }
 
-  run() {
+  run(reader, completionCallback) {
     // Until all disks are moved to last tower:
     //  1. prints stack
     //  2. ask user for move
@@ -21,21 +14,20 @@ class Game {
     this.board.display();
     
     if (this.board.isWon()) {
-      this.completionCallback();
-      reader.close();
+      completionCallback();
       return;
     }
 
-    this.askForStartIdx();
+    this.askForStartIdx(reader, completionCallback);
   }
 
-  askForStartIdx() {
+  askForStartIdx(reader, completionCallback) {
     reader.question("Please enter tower number where you want to move disk from: ", (res) => {
       let startTower = parseInt(res);
       // check input to be between 1 and 3
       if (isNaN(startTower) || startTower < 1 || startTower > 3 ) {
         console.log("Please enter number between 1 to 3");
-        this.askForStartIdx();
+        this.askForStartIdx(reader, completionCallback);
         return;
       }
 
@@ -44,24 +36,24 @@ class Game {
       // check if selected tower has disk to move 
       if (this.board.towers[startIdx].length === 0) {
         console.log(`Tower ${startTower} is empty`);
-        this.askForStartIdx();
+        this.askForStartIdx(reader, completionCallback);
         return;
       }
       // ask for tower to move to
-      this.askForEndIdx(startIdx, (startIdx, endIdx) => {
+      this.askForEndIdx(reader, startIdx, completionCallback, (startIdx, endIdx) => {
         this.board.moveDisk(startIdx, endIdx);
-        this.run();
+        this.run(reader, completionCallback);
       }); 
     });
   }
 
-  askForEndIdx(startIdx, cb) {
+  askForEndIdx(reader, startIdx, completionCallback, cb) {
     reader.question("Please enter tower number where you want to move disk to: ", (res) => {
       let endTower = parseInt(res);
 
       if (isNaN(endTower) || endTower < 1 || endTower > 3 ) {
         console.log("Please enter number between 1 to 3");
-        this.askForEndIdx(startIdx, cb);
+        this.askForEndIdx(reader, startIdx, completionCallback, cb);
         return;
       }
 
@@ -69,18 +61,13 @@ class Game {
 
       if (!this.board.isValidMove(startIdx, endIdx)) {
         console.log("Invalid move");
-        this.askForStartIdx();
+        this.askForStartIdx(reader, completionCallback);
         return;
       }
 
       cb(startIdx, endIdx); 
     });
   }
-
-  completionCallback() {
-    console.log("You did it!!!");
-  }
 }
 
-let game = new Game;
-game.run();
+module.exports = Game;
