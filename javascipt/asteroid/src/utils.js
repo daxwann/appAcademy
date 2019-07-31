8a6874ba489dc785aa34f9f595ae2a14073ae1a0
Dax Wann <daxwann@gmail.com>
2019-07-31 16:33:09 -0700

    add inherits functin in Utils POJO

--- javascipt/asteroid/src/utils.js
+++ javascipt/asteroid/src/utils.js
@@ -1,8 +1,9 @@
 // utility code, esp vector stuff
 
 const Utils = {
-  inherits(parentClass) {
-    
+  inherits: function inherits(childClass, parentClass) {
+    childClass.prototype = Object.create(parentClass.prototype);
+    childClass.prototype.constructor = childClass;
   }
 }
 
