002c572d9e744b33fbadf86a21b3843c36438d7f
Dax Wann <daxwann@gmail.com>
2019-08-15 17:34:37 -0700

    add segment test

--- javascipt/jquery_snake/test/segment.test.js
+++ javascipt/jquery_snake/test/segment.test.js
@@ -1,5 +1,44 @@
 const Segment = require("../src/segment.js");
 
 describe("Segment class", () => {
-  
+  const head = new Segment([50, 50], [0, 0]);
+
+  test("new head has no prev or next segment, no direction", () => {
+    expect(head).toEqual(
+      {
+        pos: [50, 50],
+        dir: [0, 0],
+        prev: null,
+        next: null
+      }
+    )
+  });
+
+  test("Segment#changeDir(dir) updates the dir of segment", () => {
+    head.changeDir([1, 0]);
+    expect(head.dir).toEqual(
+      [1, 0]
+    )
+  });
+
+  test("Segment#move changes the pos based on dir", () => {
+    head.move();
+    expect(head.pos).toEqual(
+      [51, 50]
+    )
+  });
+
+  test("Segment#addNext adds new segment opposite to the previous segment's direction", () => {
+    head.addNext();
+
+    const seg2 = head.next;
+    expect(seg2).toEqual(
+      {
+        pos: [50, 50],
+        dir: [1, 0],
+        prev: head,
+        next: null
+      }
+    );
+  });
 })
